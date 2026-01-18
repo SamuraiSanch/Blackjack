7f27d41f69c04345a2a292b3d3faaabcaa7ad3ba
Alex <sashakopiy@icloud.com>
2026-01-18 20:47:54 +0200

    Finished game logic. Final improvements.

--- finaltest66/blackjack.cpp
+++ finaltest66/blackjack.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include <array>
-
+#include <vector>
 enum Suit {
     CLUBS,
     DIAMONDS,
@@ -48,10 +48,10 @@ void printCard(const Card &card) {
     case ACE: std::cout << 'A'; break;
     }
     switch (card.suit) {
-    case CLUBS: std::cout << 'C'; break;
-    case DIAMONDS: std::cout << 'D'; break;
-    case HEARTS: std::cout << 'H'; break;
-    case SPADES: std::cout << 'S'; break;
+    case CLUBS: std::cout << "C "; break;
+    case DIAMONDS: std::cout << "D "; break;
+    case HEARTS: std::cout << "H "; break;
+    case SPADES: std::cout << "S "; break;
     }
 }
 
@@ -92,16 +92,68 @@ int getCardValue(const Card &card) {
     case ACE: return 11;
     }
 }
+void printPlayerCards(const std::vector<Card> &playerCards) {
+    for (const auto& card : playerCards) {
+        printCard(card);
+    }
+}
+int getTotalValueOfCards(const std::vector<Card>& playerCards) {
+    int sum = 0;
+    for (const auto& card : playerCards) {
+        sum += getCardValue(card);
+    }
+    return sum;
+}
 bool playBlackjack(const std::array<Card,52> &deck) {
+    std::cout << "Welcome to BlackJack(21) game!";
+    std::vector<Card> dealer;
     const Card* ptr = &deck[0];
-    printCard(*ptr);
+    dealer.push_back(*ptr);
+    std::cout << "\nDealer cards now is: ";
+    printPlayerCards(dealer);
+    std::cout << "\nTotal: " << getTotalValueOfCards(dealer);
+    std::cout << "\nYour cards: ";
+    std::vector<Card> player;
+    player.push_back(*(++ptr));
+    player.push_back(*(++ptr));
+    printPlayerCards(player);
+    std::cout << "\nTotal: " << getTotalValueOfCards(player);
+    char answer;
+    do {
+        std::cout << "\nDo you want to take a card?: ";
+        std::cin >> answer;
+        if (answer == 'Y' || answer == 'y') {
+            player.push_back(*(++ptr));
+            std::cout << "\nYou got a card: "; printCard(*ptr);
+            std::cout << "\nYour cards now: "; printPlayerCards(player);
+            int total = getTotalValueOfCards(player);
+            std::cout << "\nTotal: " << total;
+            if (total > 21)
+                return false;
+        }
+    } while (answer != 'N' && answer != 'n');
+    do {
+        std::cout << "\nDealer gets a card...";
+        dealer.push_back(*(++ptr));
+        std::cout << "\nDealer got a card: "; printCard(*ptr);
+        std::cout << "\nDealer cards now: "; printPlayerCards(dealer);
+        int total = getTotalValueOfCards(dealer);
+        std::cout << "\nTotal: " << total;
+        if (total > 21) 
+            return true;
+    } while (getTotalValueOfCards(dealer) < 17);
+    if (getTotalValueOfCards(dealer) > getTotalValueOfCards(player))
+        return false;
+    else if (getTotalValueOfCards(dealer) < getTotalValueOfCards(player))
+        return true;
+    else
+        std::cout << "\nDraw! Wow! We'll assume you won!";
     return true;
 }
 int main()
 {
     srand(static_cast<unsigned int>(time(0)));
     rand();
-    // Deck initialization
     std::array<Card, 52> deck;
     int card = 0;
     for (int suit = 0; suit < GENERAL_SUITS; ++suit)
@@ -111,16 +163,19 @@ int main()
             deck[card].value = static_cast<Value>(rank);
             ++card;
         }
-    /*
-    Card c{ CLUBS,TEN };
-    Card d{ HEARTS,ACE };
-    printCard(c);
-    swapCard(c, d);
-    printCard(c);
-    */
-    shuffleDeck(deck);
-    printDeck(deck);
-    playBlackjack(deck);
-
+    char userAnswer;
+    do {
+        shuffleDeck(deck);
+        printDeck(deck);
+        if (playBlackjack(deck)) {
+            std::cout << "\nYou won! Do you want to play again?(Y/N): ";
+            std::cin >> userAnswer;
+        }
+        else {
+            std::cout << "\nYou lost... Do you want to play again?(Y/N): ";
+            std::cin >> userAnswer;
+        }
+    } while (userAnswer != 'N' && userAnswer != 'n');
+    std::cout << "\nThank you for gaming!";
     return 0;
 }
